8671a3eb21766bda2d8faaa43ccc182314704227
fanteality <1454001183@qq.com>
2020-08-24 22:47:22 +0800

    :rocket: 尝试启动webpackDevServer

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,32 @@
-'use strict'
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const mode = process.env.NODE_ENV === 'development' ? mode : 'production'
+console.log(mode, __dirname)
 module.exports = {
-  entry: {},
-  module: {},
-  plugins: {},
-  output: {}
+  mode,
+  entry: './src',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: ['vue-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    port: 8080,
+    open: 'Google Chrome',
+    contentBase: './dist',
+    compress: true
+  }
 }
