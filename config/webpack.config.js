3e39ebd84f9a7c8bd090af08f1fd3a666603fd64
fanteality <1454001183@qq.com>
2020-08-24 18:24:01 +0800

    :wrench: 初期配置

---
+++ config/webpack.config.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  entry: {},
+  module: {},
+  plugins: {},
+  output: {}
+}
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        name: 'lcf'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .box {
+    height: 100vh;
+    background-color: #cecece;
+  }
+</style>
